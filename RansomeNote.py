2ec096dcf56a45c7430b83858ba1a8f41fefed21
yogeshPbawankar <yogesh60344@gmail.com>
2023-08-17 11:30:41 +0530

    The problem Ransom Note Solution

---
+++ RansomeNote.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+
+        # importing the counter module from collections
+        from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # counter is used to count no of occurance.
+
+        magazine_count = Counter(magazine)
+        ransomNote_count = Counter(ransomNote)
+        
+        #it checks if the charecter eixt or less than count.
+        for char, count in ransomNote_count.items():
+            if char not in magazine_count or magazine_count[char] < count:
+                return False        
+        return True
+
+        
+solution = Solution()
+
+ransomNote1 = "aa"
+magazine1 = "aab"
+print(solution.canConstruct(ransomNote1, magazine1))
